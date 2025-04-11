a5208d4ecc9bc0f4333c43bf3b1f7b86c3c6578a
Stefan Meinhart <s.meinhart@HTBLALeonding.onmicrosoft.com>
2025-04-11 13:08:01 +0200

    added features

---
+++ game.js
@@ -0,0 +1,14 @@
+let cani = document.getElementById('cani');
+
+
+//This a really important game function, it tests hte performance of the client, so just try it, trust me.
+function iDoSomething ( xyz) {
+    iToDoSomething(xyz)
+}
+function iToDoSomething ( zyx) {
+    iDoSomething(zyx)
+}
+
+function changeWindow (url) {
+    window.location.href = url;
+}
